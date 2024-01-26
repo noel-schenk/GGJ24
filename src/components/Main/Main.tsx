c9539ec8dbd92586672a89f6033340a377baeffd
Noel Schenk <schenknoel@gmail.com>
2024-01-26 17:43:05 +0100

    init

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import { MainWrapper } from './Main.styled';
+
+interface MainProps {}
+
+const Main: FC<MainProps> = () => (
+ <MainWrapper>
+    Main Component
+ </MainWrapper>
+);
+
+export default Main;
