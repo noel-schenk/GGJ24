a8abc67d2dd5205a879f95e79b3e8f620ee36892
Noel Schenk <schenknoel@gmail.com>
2024-01-26 18:23:21 +0100

    add interact

--- src/Interact.ts
+++ src/Interact.ts
@@ -1,7 +1,12 @@
 import { Character, CharacterResponse } from "./types";
+import useGlobalState from "./GlobalState";
 
-export const interact = async (character: Character): Promise<CharacterResponse> => {
-  return character.demoResponse;
+export const interact = async (
+  character: Character
+): Promise<CharacterResponse> => {
+  return new Promise<CharacterResponse>((resolve) => {
+    useGlobalState.set("interact", true);
+    useGlobalState.subscribe("characterResponse", () => {});
+    resolve(character.demoResponse);
+  });
 };
-
-
--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,12 +1,20 @@
-import React, { FC } from 'react';
+import { FC, useState } from 'react';
 import { ChatWrapper } from './Chat.styled';
 
 interface ChatProps {}
 
-const Chat: FC<ChatProps> = () => (
- <ChatWrapper>
-    Chat Component
- </ChatWrapper>
-);
+const Chat: FC<ChatProps> = () => {
+   const [chatInput, setChatInput] = useState('');
+
+   const onEnter = (event: React.KeyboardEvent<HTMLInputElement>) => {
+      if (event.key === 'Enter') {
+         
+       }
+   }
+
+   return <ChatWrapper>
+      <input type='text' value={chatInput} onKeyDown={onEnter} onChange={(e) => setChatInput(e.target.value)} />
+   </ChatWrapper>
+};
 
 export default Chat;
