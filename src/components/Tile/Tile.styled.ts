5dd6e4b284389a20826abfdf7ba0397477c02e3d
Jan Sobotta <cv.jsobotta@googlemail.com>
2024-01-27 00:23:08 +0100

    finalize

--- src/components/Tile/Tile.styled.ts
+++ src/components/Tile/Tile.styled.ts
@@ -5,18 +5,23 @@ export const TileWrapper = styled.div`
     width: 1em;
     height: 1em;
     position: absolute;
-    contain: strict;
-    content-visibility: auto;
-
+    // contain: strict;
+    // content-visibility: auto;
     background-size: 10em;
 `;
 
 export const MessageWrapper = styled.div`
-    width: 1em;
-    height: 1em;
     position: absolute;
-    contain: strict;
-    content-visibility: auto;
-
-    background-size: 10em;
+    font-size: 2vh;
+    background: white;
+    border: 2px solid black;
+    padding: 0.7vh;
+    border-radius: 1vh;
+    width: 20vw;
+    max-width: 50vw;
+    max-height: 30vh;
+    overflow:hidden;
+    left: 50%;
+    top: 0;
+    transform: translateX(-50%) translateY(-100%);
 `;
