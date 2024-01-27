574f1f57d5a83f5d73361c15aa1689ac0fe7d7bb
Noel Schenk <schenknoel@gmail.com>
2024-01-27 18:46:51 +0100

    add initials demo map

--- src/data/DemoMap.ts
+++ src/data/DemoMap.ts
@@ -1,29 +1,30 @@
 export const DemoMap = `
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-bbbbbbbb  *  6      b       bbbbbbbbb
-bbbbbbb             b  bb   bbbbbbbbb
-bbbbbb   bbbbb     bbbbb     bbbbbbbb
-bbbbb  *         bbbbbbb 5    bbbbbbb
-bbbbb     *     bbbbbbbb  *   bbbbbbb
-bbbbb987           bbbbbb    bbbbbbbb
-bbbbb         *     bb      bbbbbbbbb
-bbbbb *  /\\                  bbbbbbbb
-bbbbb   /||\\    *   /\\   *   bbbbbbbb
-bbbbb   |==|       /||\\     bbbbbbbbb
-bbbbb   |=#|       |==|    bbbbbbbbbb
-bbbbb            3g|=#|4    bbbbbbbbb
-bbbbmmm     *       s    *   bbbbbbbb
-bbbbmmmm            t 1     bbbbbbbbb
-bbbbm mmmm     *            bbbbbbbbb
-bbbbm * mmmmmmm       b    bbbbbbbbbb
-bbbbm     mmmmm   *    bbbbbbbbbbbbbb
-bbbbm   *     mm          bbbbbbbbbbb
-bbbbm *        m 0        bbbbbbbbbbb
-bbbbmm  2  *             bbbbbbbbbbbb
-bbbbmmm        m     bbbbbbbbbbbbbbbb
-bbbbbmmmmmmmmmmmbbbbbbbbbbbbbbbbbbbbb
-bbbbbbbmmmmmmbbbbbbbbbbbbbbbbbbbbbbbb
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbbbbb  b  6      b       bbbbbbbbbbbbb  bbbb    bbbb  b  bbbb  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbbbb             b  bb   bbbbbbbbbbbbb  bbbb    bbbb  b  bbbb  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbbb   bbbbb     bbbbb     bbbbbbbbbbbb  bbb  bb  bbb  bb  bbb  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbb  b         bbbbbbb 5    bbbbbbbbbbb  bb  bbbb  bb  bbb  bb  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbb     b     bbbbbbbb  b   bbbbbbbbbbb  bb        bb  bbbb  b  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbb987           bbbbbb    bbbbbbbbbbbb  bb  bbbb  bb  bbbbb    bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbb         b     bb      bbbbbbbbbbbbb  bb  bbbb  bb  bbbbbb   bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbb b   \\                  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbb    ||\\    b    \\   b   bbbbbbbbbb      bbbbb       bbb  bbbbbbb  b      bbbbb       bbb  bbbbbbb  bbbb
+bbbbb   |==|        ||\\     bbbbbbbbbbbb bbbb  bbb  bbbbb  bb  bbbbbbb  b bbbb  bbb  bbbbb  bb  bbbbbbb  bbbb
+bbbbb   |=#|       |==|    bbbbbbbbbbbbbb bbbb  bb  bbbbbbb  b  bbb bbb  b bbbb  bb  bbbbbbb  b  bbb bbb  bbbb
+bbbbb            3g|=#|4    bbbbbbbbbbbbb      bbb  bbbbbbb  b  bb   bb  b      bbb  bbbbbbb  b  bb   bb  bbbb
+bbbbmmm     b       s    b   bbbbbbbbbbbb bbbbb  b  bbbbbbb  b  b  b  b  b bbbbb  b  bbbbbbb  b  b  b  b  bbbb
+bbbbmmmm            t 1     bbbbbbbbbbbbb bbbbb  bb  bbbbb  bb    bbb    b bbbbb  bb  bbbbb  bb    bbb    bbbb
+bbbbm mmmm     b            bbbbbbbbbbbbb       bbbb       bbb  bbbbbbb  b       bbbb       bbb  bbbbbbb  bbbb
+bbbbm b mmmmmmm       b    bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbm     mmmmm   b    bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbm   b     mm          bbbbbbbbbbbbbbb    bbbbb  bbb       bbb        b  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbm b        m 0        bbbbbbbbbbbbbbb  b  bbbb  bb  bbbbb  bb  bbbbbbb  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbmm  2  b             bbbbbbbbbbbbbbbb  bb  bbb  b  bbbbbbb  b  bbbbbbb  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbmmm        m     bbbbbbbbbbbbbbbbbbbb  bbb  bb  b  bbbbbbb  b       bb  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbbmmmmmmmmmmmbbbbbbbbbbbbbbbbbbbbbbbbb  bbbb  b  b  bbbbbbb  b  bbbbbbb  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbbbbmmmmmmbbbbbbbbbbbbbbbbbbbbbbbbbbbb  bbbbb    bb  bbbbb  bb  bbbbbbb  bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb  bbbbbb   bbb       bbb        b        bbbbbbbbbbbbbbbbbbbbbbbbbbbb
+bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
 `;
