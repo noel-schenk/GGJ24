f4acb574e7c84826d328efe651a0f0259ab3b695
Noel Schenk <schenknoel@gmail.com>
2024-01-27 12:22:55 +0100

    fix tts

--- src/TTS.ts
+++ src/TTS.ts
@@ -1,12 +1,12 @@
-import useGlobalState from "./GlobalState";
-
 export const playTTS = (message: string, voice = "Google Nederlands") => {
-  const voices = window.speechSynthesis.getVoices();
-  const utterThis = new SpeechSynthesisUtterance(message);
-  utterThis.pitch = useGlobalState.get("activeCharacter").pitch | 1;
-  utterThis.rate = useGlobalState.get("activeCharacter").rate | 1;
-  utterThis.voice = voices.find(
-    (ssvoice) => ssvoice.name === voice
-  ) as SpeechSynthesisVoice;
-  window.speechSynthesis.speak(utterThis);
+  if ("speechSynthesis" in window) {
+    const voices = window.speechSynthesis.getVoices();
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(message);
+
+    utterance.voice = voices.find(
+      (ssvoice) => ssvoice.name === voice
+    ) as SpeechSynthesisVoice;
+    synth.speak(utterance);
+  }
 };
