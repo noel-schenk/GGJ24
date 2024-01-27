0c3dbef12967d778474f6c63d259375bdcfc9d34
Noel Schenk <schenknoel@gmail.com>
2024-01-27 12:08:17 +0100

    add local tts

--- src/TTS.ts
+++ src/TTS.ts
@@ -1,12 +1,12 @@
 import useGlobalState from "./GlobalState";
 
-export const playTTS = (message: string, voice = "alloy") => {
-  const voices = speechSynthesis.getVoices();
+export const playTTS = (message: string, voice = "Google Nederlands") => {
+  const voices = window.speechSynthesis.getVoices();
   const utterThis = new SpeechSynthesisUtterance(message);
   utterThis.pitch = useGlobalState.get("activeCharacter").pitch | 1;
   utterThis.rate = useGlobalState.get("activeCharacter").rate | 1;
   utterThis.voice = voices.find(
     (ssvoice) => ssvoice.name === voice
   ) as SpeechSynthesisVoice;
-  speechSynthesis.speak(utterThis);
+  window.speechSynthesis.speak(utterThis);
 };
