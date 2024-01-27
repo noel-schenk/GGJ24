9dd84fc90ae1a2c2fd53c57300a7cf83faabf462
Jan Sobotta <cv.jsobotta@googlemail.com>
2024-01-27 02:02:39 +0100

    map push

--- src/data/DemoMap.ts
+++ src/data/DemoMap.ts
@@ -5,17 +5,17 @@ export const DemoMap = `
 bb   bbbbb     bbbb     b
 b    m g t 1      b  4  b
 b                 b     b
-b      s    0     bbb   b
+b  7   s    0     bbb   b
 b                       b
 b    /\\                 b
 b   /||\\                b
 b   |==|                b
-b   |=#|3               b
+b   |=#|3         5     b
 b                       b
 mm                      b
 mmm                     b
 m mmm                   b
-m   mmm                 b
+m   mmm         6       b
 m     mmmmm             b
 m         mm            b
 m          m            b
